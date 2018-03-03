95e40bdd83d4cb8ae2588824393de7d9b25d51e8
Amith Kini <kini.amit.22@gmail.com>
2018-03-04 00:31:27 +0530

    Add Django application

---
+++ car_dealership/web/urls.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+from . import views
+
+app_name = 'web'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login_user, name='login'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ car_dealership/web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'web/index.html')
+
+
+def login_user(request):
+    return render(request, 'web/login.html')
